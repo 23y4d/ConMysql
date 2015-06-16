7e4b1ac1022af0c3e6889439f9f75bd1336a08b0
ZEYAD BESISO <zeyad.besiso@gmail.com>
2015-06-16 15:33:30 +0300

    ConMysql

---
+++ confing.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * A very simple  Connect to MySqL
+ *
+ * @author  Zeyad Besiso <zeyad.besiso@gmail.com>
+ *
+ */
+
+//
+// Thrown when an error occurs.
+//
+class errorsServerException extends Exception {
+    // custom string representation of object
+    public function __toString() {
+        if ($this->code) {
+            return "[{$this->code}] {$this->message}\n";
+        } else {
+            return "{$this->message}\n";
+
+        }
+    }
+}
+
+
+
+class ConMysql{
+
+    private $dbhost,$dbuser,$dbpass,$db;
+
+
+    function __construct($host,$user,$pass,$db){
+
+        $this->dbhost = $host;
+        $this->dbuser = $user;
+        $this->dbpass = $pass;
+        $this->db     = $db;
+        /**
+         * @throws errors
+         *
+         */
+            try {
+                $this->Connect();
+                $this->selectDb();
+           	} catch (errorsServerException $why) {
+        	    echo $why->getMessage();
+        	    }
+    }
+    /**
+	 * make Connect
+	 *
+	 * @access private
+	 * @return static
+	 */
+    private function Connect() {
+
+        $sql =  @mysql_connect($this->dbhost,$this->dbuser,$this->dbpass);
+        if(!$sql) throw new errorsServerException("<pre class='preC' > Error => Not Connect !</pre>");
+        else return true;
+
+    }
+    /**
+	 * make selectDb
+	 *
+	 * @access private
+	 * @return static
+	 */
+    private function selectDb() {
+
+        $sql = @mysql_select_db($this->db);
+        if(!$sql) throw new errorsServerException("<pre class='preC'> Error => NO DATABASE SELECTED !</pre>");
+        else return true;
+
+    }
+
+
+
+}
+//run class
+ new ConMysql("localhost","root","root","app");
